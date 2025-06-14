3c01bc4ae68012b5ada39ee85135a2fd8bb72574
nuralom787 <md9577630@gmail.com>
2025-06-14 20:13:00 +0600

    update codebase and remove unused or others apis

--- index.js
+++ index.js
@@ -91,115 +91,16 @@ async function run() {
 
 
         // Check User Admin Middleware.
-        const VerifyAdmin = async (req, res, next) => {
-            //     const email = req.decoded.email;
-            //     const query = { email: email };
-            //     const user = await UsersCollection.findOne(query);
-            //     const isAdmin = user?.role === 'admin';
-            //     if (!isAdmin) {
-            //         return res.status(403).send({ message: 'forbidden access' });
-            //     }
-            //     next();
-        };
-
-
-
-
-        // -----------------------------------------------------------
-        //                      Dashboard Stats
-        // -----------------------------------------------------------
-
-
-
-
-        app.get("/dashboard/order-stats", async (req, res) => {
-
-            // Get today's start and end time
-            const todayStart = moment().startOf("day").toDate();
-            const todayEnd = moment().endOf("day").toDate();
-
-            // Get yesterday's start and end time
-            const yesterdayStart = moment().subtract(1, "days").startOf("day").toDate();
-            const yesterdayEnd = moment().subtract(1, "days").endOf("day").toDate();
-
-            // Get first day and last day of current month
-            const monthStart = moment().startOf("month").toDate();
-            const monthEnd = moment().endOf("month").toDate();
-
-            // Get last month's start and end time
-            const lastMonthStart = moment().subtract(1, "months").startOf("month").toDate();
-            const lastMonthEnd = moment().subtract(1, "months").endOf("month").toDate();
-
-            // Aggregate for today's revenue
-            const todayRevenue = await ordersCollection.aggregate([
-                { $match: { orderTime: { $gte: todayStart, $lte: todayEnd } } },
-                { $group: { _id: null, total: { $sum: "$grandTotal" } } }
-            ]).toArray();
-
-            // Aggregate for yesterday's revenue
-            const yesterdayRevenue = await ordersCollection.aggregate([
-                { $match: { orderTime: { $gte: yesterdayStart, $lte: yesterdayEnd } } },
-                { $group: { _id: null, total: { $sum: "$grandTotal" } } }
-            ]).toArray();
-
-            // Aggregate for method's revenue
-            const methodRevenue = await ordersCollection.aggregate([
-                { $group: { _id: "$paymentMethod.type", totalAmount: { $sum: "$grandTotal" } } },
-                {
-                    $project: {
-                        _id: 0,
-                        name: "$_id",
-                        totalAmount: "$totalAmount"
-                    }
-                }
-            ]).toArray();
-
-            // Aggregate for this month's revenue
-            const thisMonthRevenue = await ordersCollection.aggregate([
-                { $match: { orderTime: { $gte: monthStart, $lte: monthEnd } } },
-                { $group: { _id: null, total: { $sum: "$grandTotal" } } }
-            ]).toArray();
-
-            // Aggregate for last month's revenue
-            const lastMonthRevenue = await ordersCollection.aggregate([
-                { $match: { orderTime: { $gte: lastMonthStart, $lte: lastMonthEnd } } },
-                { $group: { _id: null, total: { $sum: "$grandTotal" } } }
-            ]).toArray();
-
-            // Aggregate for all-time revenue
-            const allTimeRevenue = await ordersCollection.aggregate([
-                { $group: { _id: null, total: { $sum: "$grandTotal" } } }
-            ]).toArray();
-
-
-            // Get Total and status wise orders.
-            const totalOrders = (await ordersCollection.find().toArray()).length;
-
-            // 
-            const statusCounts = await ordersCollection.aggregate([
-                { $group: { _id: "$status", count: { $sum: 1 } } },
-                {
-                    $project: {
-                        _id: 0,
-                        status: "$_id",
-                        count: "$count"
-                    }
-                }
-            ]).toArray();
-
-            res.send({
-                todayRevenue: todayRevenue.length > 0 ? todayRevenue[0].total : 0,
-                yesterdayRevenue: yesterdayRevenue.length > 0 ? yesterdayRevenue[0].total : 0,
-                methodRevenue,
-                thisMonthRevenue: thisMonthRevenue.length > 0 ? thisMonthRevenue[0].total : 0,
-                lastMonthRevenue: lastMonthRevenue.length > 0 ? lastMonthRevenue[0].total : 0,
-                allTimeRevenue: allTimeRevenue.length > 0 ? allTimeRevenue[0].total : 0,
-                totalOrders,
-                statusCounts
-            });
-        });
-
-
+        // const VerifyAdmin = async (req, res, next) => {
+        //     //     const email = req.decoded.email;
+        //     //     const query = { email: email };
+        //     //     const user = await UsersCollection.findOne(query);
+        //     //     const isAdmin = user?.role === 'admin';
+        //     //     if (!isAdmin) {
+        //     //         return res.status(403).send({ message: 'forbidden access' });
+        //     //     }
+        //     //     next();
+        // };
 
 
 
@@ -273,106 +174,10 @@ async function run() {
             });
         });
 
-        // Add/Post New Products.
-        app.post('/add-new/product', async (req, res) => {
-            const product = req.body;
-
-            const price = parseInt(product.price);
-            const originalPrice = parseInt(product.originalPrice);
-            const quantity = parseInt(product.quantity);
-            product.price = price;
-            product.originalPrice = originalPrice;
-            product.quantity = quantity;
-            product.discount = (originalPrice - price) / originalPrice * 100;
-
-            product.status = "Show";
-            product.createdAt = new Date().toISOString();
-            product.updatedAt = new Date().toISOString();
-            const result = await productsCollection.insertOne(product);
-            res.send(result);
-        });
-
-
-        // Update Product.
-        app.patch('/update/product/:id', async (req, res) => {
-            const id = req.params.id;
-            const product = req.body;
-            const price = parseInt(product.price);
-            const originalPrice = parseInt(product.originalPrice);
-            const quantity = parseInt(product.quantity);
-            product.price = price;
-            product.originalPrice = originalPrice;
-            product.quantity = quantity;
-            product.discount = (originalPrice - price) / originalPrice * 100;
-            product.updatedAt = new Date().toISOString();
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    price: product.price,
-                    discount: product.discount,
-                    tag: product.tag,
-                    flashSale: product.flashSale,
-                    children: product.children,
-                    description: product.description,
-                    image: product.image,
-                    thumb: product.thumb,
-                    originalPrice: product.originalPrice,
-                    parent: product.parent,
-                    quantity: product.quantity,
-                    slug: product.slug,
-                    title: product.title,
-                    type: product.type,
-                    unit: product.unit,
-                    updatedAt: product.updatedAt,
-                    sku: product.sku
-                }
-            };
-            const result = await productsCollection.updateOne(filter, updateDoc);
-            res.json(result);
-        });
-
-
-        // Update Product Status.
-        app.patch('/update/product-status/:id', async (req, res) => {
-            const id = req.params.id;
-            const currentStatus = req.body;
-            if (currentStatus.status === "Show") {
-                currentStatus.status = "Hide";
-            } else {
-                currentStatus.status = "Show";
-            }
-            const updatedAt = new Date().toISOString();
-
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    status: currentStatus.status,
-                    updatedAt: updatedAt
-                }
-            };
-
-            const result = await productsCollection.updateOne(filter, updateDoc);
-            res.json(result);
-        });
-
-
-        // Delete Product.
-        app.delete('/product/delete/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await productsCollection.deleteOne(query);
-            res.send(result);
-        });
-
-
-
-
 
         /*---------------------------------------------------------
         //                      Categories API.
         ---------------------------------------------------------*/
-
-
 
 
 
@@ -421,71 +226,6 @@ async function run() {
         });
 
 
-        // Post New Categories API.
-        app.post('/add-new/category', async (req, res) => {
-            const category = req.body;
-            // console.log(category);
-            const result = await categoriesCollection.insertOne(category);
-            res.send(result);
-        });
-
-
-        // Update Category.
-        app.patch('/update/category/:id', async (req, res) => {
-            const id = req.params.id;
-            const category = req.body;
-
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    children: category.children,
-                    status: category.status,
-                    parent: category.parent,
-                    type: category.type,
-                    icon: category.icon,
-                    thumb: category.thumb ? category.thumb : null
-                }
-            };
-
-            const result = await categoriesCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        });
-
-
-        // Update Category Status.
-        app.patch('/update/category-status/:id', async (req, res) => {
-            const id = req.params.id;
-            const currentStatus = req.body;
-            if (currentStatus.status === "Show") {
-                currentStatus.status = "Hide";
-            } else {
-                currentStatus.status = "Show";
-            }
-
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    status: currentStatus.status
-                }
-            };
-
-            const result = await categoriesCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        });
-
-
-        // Delete Category.
-        app.delete('/category/delete/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await categoriesCollection.deleteOne(query);
-            res.send(result);
-        });
-
-
-
-
-
 
 
         /*--------------------------------------------------------------
@@ -531,49 +271,6 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await couponsCollection.findOne(query);
-            res.send(result);
-        });
-
-
-
-        // Post New Coupon.
-        app.post('/add-new/coupon', async (req, res) => {
-            const coupon = req.body;
-            const result = await couponsCollection.insertOne(coupon);
-            res.send(result);
-        });
-
-
-        // Update Coupons.
-        app.patch('/update/coupon/:id', async (req, res) => {
-            const id = req.params.id;
-            const data = req.body;
-
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    logo: data.logo,
-                    thumb: data.thumb,
-                    title: data.title,
-                    minimumAmount: data.minimumAmount,
-                    discountPercentage: data.discountPercentage,
-                    productType: data.productType,
-                    couponCode: data.couponCode,
-                    endTime: data.endTime,
-                    updatedAt: new Date().toISOString()
-                }
-            };
-
-            const result = await couponsCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        });
-
-
-        // Delete coupons.
-        app.delete('/coupon/delete/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await couponsCollection.deleteOne(query);
             res.send(result);
         });
 
@@ -938,20 +635,20 @@ async function run() {
 
 
         // Update Order Status.
-        app.patch('/update/order-status/:id', async (req, res) => {
-            const id = req.params.id;
-            const currentStatus = req.body;
+        // app.patch('/update/order-status/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const currentStatus = req.body;
 
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    status: currentStatus.status
-                }
-            };
+        //     const filter = { _id: new ObjectId(id) };
+        //     const updateDoc = {
+        //         $set: {
+        //             status: currentStatus.status
+        //         }
+        //     };
 
-            const result = await ordersCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        });
+        //     const result = await ordersCollection.updateOne(filter, updateDoc);
+        //     res.send(result);
+        // });
 
 
         // 
@@ -1123,130 +820,6 @@ async function run() {
             // console.log(id)
             const query = { _id: new ObjectId(id) };
             const result = await customersCollection.deleteOne(query);
-            res.send(result);
-        });
-
-
-        /*---------------------------------------------------------------
-        //                          Staffs API.
-        ---------------------------------------------------------------*/
-
-
-        // Get All Staffs.
-        app.get('/staffs', async (req, res) => {
-            const page = req.query.page;
-            const size = parseInt(req.query.size);
-            const email = req.query.email;
-            const search = req.query.search;
-            const role = req.query.role;
-
-            let staffs;
-            let count;
-            if (page) {
-                const filter = {
-                    ...(search && {
-                        $or: [
-                            { displayName: { $regex: search, $options: 'i' } },
-                            { email: { $regex: search, $options: 'i' } },
-                            { contact: { $regex: search, $options: 'i' } }
-                        ]
-                    }),
-                    ...(email && { email: { $regex: email, $options: 'i' } }),
-                    ...(role && { role: { $regex: role, $options: 'i' } })
-                };
-                staffs = await staffsCollection.find(filter).skip(page * size).limit(size).toArray();
-                const staffsLimit = await staffsCollection.find(filter).toArray();
-                count = staffsLimit.length;
-            }
-            else {
-                const filter = {
-                    ...(search && {
-                        $or: [
-                            { displayName: { $regex: search, $options: 'i' } },
-                            { email: { $regex: search, $options: 'i' } },
-                            { contact: { $regex: search, $options: 'i' } }
-                        ]
-                    }),
-                    ...(email && { email: { $regex: email, $options: 'i' } }),
-                    ...(role && { role: { $regex: role, $options: 'i' } })
-                };
-                staffs = await staffsCollection.find(filter).toArray();
-                count = staffs.length;
-            }
-
-            const totalCount = await staffsCollection.estimatedDocumentCount();
-
-            res.send({
-                totalCount,
-                count,
-                staffs,
-            });
-        });
-
-
-        // Get a Specific Staff.
-        app.get('/staff', async (req, res) => {
-            const email = req.query.email;
-            const query = { email: email };
-            const result = await staffsCollection.findOne(query);
-            res.send(result);
-        });
-
-
-        // Check Staff Role.
-        app.get('/staff/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            let isAdmin = false;
-            const staff = await staffsCollection.findOne(query);
-            if (staff?.role === "Admin") {
-                isAdmin = true;
-            };
-            res.json({ admin: isAdmin });
-        });
-
-
-        // Post New Staffs.
-        app.post('/add-new/staff', async (req, res) => {
-            const staff = req.body;
-            const hashedPass = await argon2.hash(staff.password);
-            staff.password = hashedPass;
-            const result = await staffsCollection.insertOne(staff);
-            res.send(result);
-        });
-
-
-        // Update Staff Profile Information.
-        app.patch('/update/staff/:id', async (req, res) => {
-            const id = req.params.id;
-            const data = req.body;
-            const hashedPass = await argon2.hash(data.password);
-            data.password = hashedPass;
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    photoURL: data.photoURL,
-                    thumb: data.thumb,
-                    displayName: data.displayName,
-                    email: data.email,
-                    contact: data.contact,
-                    password: data.password,
-                    joiningDate: data.joiningDate,
-                    role: data.role,
-                    updatedAt: data.updatedAt
-                }
-            };
-
-            const result = await staffsCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        });
-
-
-        // Delete Staff Information.
-        app.delete("/staff/delete/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await staffsCollection.deleteOne(query);
             res.send(result);
         });
 
